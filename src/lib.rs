f97266ea36dd3694f8f178b5e459c284ec034087
PeterLombaers <peter@idfuse.nl>
2023-12-07 15:21:41 +0100

    Read multiple references

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use nom::branch::alt;
-use nom::bytes::complete::{is_not, tag};
+use nom::bytes::complete::{is_not, tag, take_until};
 use nom::character::complete::{line_ending, one_of};
 use nom::combinator::{map, recognize, value};
-use nom::multi::many_till;
+use nom::multi::{many_till, separated_list1};
 use nom::sequence::{pair, preceded, terminated};
 use nom::IResult;
 
@@ -82,6 +82,17 @@ fn parse_end_of_reference(input: &str) -> IResult<&str, ()> {
     value((), tag("ER  - "))(input)
 }
 
+fn parse_to_next_reference(input: &str) -> IResult<&str, &str> {
+    take_until("TY  - ")(input)
+}
+
+fn parse_ris(input: &str) -> IResult<&str, Vec<Reference>> {
+    preceded(
+        parse_to_next_reference,
+        separated_list1(parse_to_next_reference, parse_reference),
+    )(input)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -95,7 +106,7 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_reference() {
+    fn test_reference() {
         let ref_string = "TY  - JOUR
 AU  - Shannon,Claude E.
 PY  - 1948/07//
@@ -145,5 +156,87 @@ ER  - ";
                 ]
             }
         )
+    }
+
+    #[test]
+    fn test_multiple_references() {
+        let ref_string = "1.
+TY  - JOUR
+ID  - 12345
+T1  - Title of reference
+A1  - Marx, Karl
+A1  - Lindgren, Astrid
+A2  - Glattauer, Daniel
+Y1  - 2014//
+N2  - BACKGROUND: Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  RESULTS: Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. CONCLUSIONS: Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+KW  - Pippi
+KW  - Nordwind
+KW  - Piraten
+JF  - Lorem
+JA  - lorem
+VL  - 9
+IS  - 3
+SP  - e0815
+CY  - United States
+PB  - Fun Factory
+SN  - 1932-6208
+M1  - 1008150341
+L2  - http://example.com
+UR  - http://example_url.com
+ER  - 
+
+2.
+TY  - JOUR
+ID  - 12345
+T1  - The title of the reference
+A1  - Marxus, Karlus
+A1  - Lindgren, Astrid
+A2  - Glattauer, Daniel
+Y1  - 2006//
+N2  - BACKGROUND: Lorem dammed ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  RESULTS: Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. CONCLUSIONS: Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+KW  - Pippi Langstrumpf
+KW  - Nordwind
+KW  - Piraten
+JF  - Lorem
+JA  - lorem
+VL  - 6
+IS  - 3
+SP  - e0815341
+CY  - Germany
+PB  - Dark Factory
+SN  - 1732-4208
+M1  - 1228150341
+L2  - http://example2.com
+UR  - http://example_url.com
+ER  - ";
+        let (_, references) = parse_ris(ref_string).unwrap();
+        assert_eq!(references.len(), 2);
+        assert_eq!(references[0].ref_type, "JOUR".to_string());
+        assert!(references[0].fields.contains(&Field {
+            tag: "ID".to_string(),
+            content: Content::String("12345".to_string())
+        }));
+        assert!(references[0].fields.contains(&Field {
+            tag: "CY".to_string(),
+            content: Content::String("United States".to_string())
+        }));
+        assert!(references[0].fields.contains(&Field {
+            tag: "Y1".to_string(),
+            content: Content::String("2014//".to_string())
+        }));
+
+        assert_eq!(references[1].ref_type, "JOUR".to_string());
+        assert!(references[1].fields.contains(&Field {
+            tag: "T1".to_string(),
+            content: Content::String("The title of the reference".to_string())
+        }));
+        assert!(references[1].fields.contains(&Field {
+            tag: "SN".to_string(),
+            content: Content::String("1732-4208".to_string())
+        }));
+        assert!(references[1].fields.contains(&Field {
+            tag: "UR".to_string(),
+            content: Content::String("http://example_url.com".to_string())
+        }));
     }
 }
