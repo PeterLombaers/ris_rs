d7983212a799841e7fe8572950bcce318f78e4b3
PeterLombaers <peter@idfuse.nl>
2023-12-22 15:37:17 +0100

    Add full file test

---
+++ tests/appenzeller_herzog.rs
@@ -0,0 +1,11 @@
+use ris::parse_ris;
+use std::fs;
+
+
+#[test]
+fn parse() {
+    let file_path = "tests/files/Appenzeller-Herzog_2019.ris";
+    let contents = fs::read_to_string(file_path).unwrap();
+    let (_, output) = parse_ris(&contents).unwrap();
+    assert_eq!(output.len(), 3453)
+}
