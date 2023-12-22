3f1a6ecc77918e28799bbfa9f8fcee2ac6691ad0
PeterLombaers <peter@idfuse.nl>
2023-12-22 15:35:38 +0100

    Split benchmarks

--- benches/parser.rs
+++ benches/parser.rs
@@ -31,73 +31,11 @@ ER  - ";
     });
 }
 
-pub fn reference_2(c: &mut Criterion) {
-    let reference_string = "1.
-TY  - JOUR
-ID  - 12345
-T1  - Title of reference
-A1  - Marx, Karl
-A1  - Lindgren, Astrid
-A2  - Glattauer, Daniel
-Y1  - 2014//
-N2  - BACKGROUND: Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  RESULTS: Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. CONCLUSIONS: Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-KW  - Pippi
-KW  - Nordwind
-KW  - Piraten
-JF  - Lorem
-JA  - lorem
-VL  - 9
-IS  - 3
-SP  - e0815
-CY  - United States
-PB  - Fun Factory
-SN  - 1932-6208
-M1  - 1008150341
-L2  - http://example.com
-UR  - http://example_url.com
-ER  - 
-
-2.
-TY  - JOUR
-ID  - 12345
-T1  - The title of the reference
-A1  - Marxus, Karlus
-A1  - Lindgren, Astrid
-A2  - Glattauer, Daniel
-Y1  - 2006//
-N2  - BACKGROUND: Lorem dammed ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  RESULTS: Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. CONCLUSIONS: Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-KW  - Pippi Langstrumpf
-KW  - Nordwind
-KW  - Piraten
-JF  - Lorem
-JA  - lorem
-VL  - 6
-IS  - 3
-SP  - e0815341
-CY  - Germany
-PB  - Dark Factory
-SN  - 1732-4208
-M1  - 1228150341
-L2  - http://example2.com
-UR  - http://example_url.com
-ER  - ";
-    c.bench_function("reference", |b| b.iter(|| parse_ris(&reference_string)));
-}
-
-pub fn parse_file(file_path: &str) {
-    let contents = fs::read_to_string(file_path).unwrap();
-    parse_ris(&contents).unwrap();
-}
-
 pub fn appenzeller_herzog(c: &mut Criterion) {
     let file_path = "benches/files/Appenzeller-Herzog_2019.ris";
-    c.bench_function("appenzeller_herzog", |b| b.iter(|| parse_file(&file_path)));
+    let contents = fs::read_to_string(file_path).unwrap();
+    c.bench_function("appenzeller_herzog", |b| b.iter(|| parse_ris(&contents)));
 }
 
-pub fn kwok(c: &mut Criterion) {
-    let file_path = "benches/files/Kwok_2020.ris";
-    c.bench_function("kwok", |b| b.iter(|| parse_file(&file_path)));
-}
-
-criterion_group!(benches, reference, reference_2, appenzeller_herzog, kwok);
+criterion_group!(benches, reference, appenzeller_herzog);
 criterion_main!(benches);
