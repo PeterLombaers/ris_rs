e07b87eba6d87e4628a417d9085fea450c2a895f
PeterLombaers <peter@idfuse.nl>
2024-02-01 15:46:18 +0100

    Remove winnow parse and rename handwritten

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,10 @@ mod field;
 mod nom_parser;
 mod reference;
 mod tag;
-// mod winnow_parser;
-mod handwritten;
+mod parser;
 
 pub use field::Field;
-pub use handwritten::RisParser;
+pub use parser::RisParser;
 pub use nom_parser::parse_ris as parse_ris_nom;
 pub use reference::Reference;
 pub use tag::Tag;
