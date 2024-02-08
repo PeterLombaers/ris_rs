8e9eec37a7e27e0d67225affe97d0b9f030acaee
PeterLombaers <peter@idfuse.nl>
2024-02-08 14:08:51 +0100

    Remove nom parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
-mod field;
-mod nom_parser;
-mod reference;
-mod tag;
 mod parser;
 mod python_bindings;
 
-pub use field::Field;
 pub use parser::RisParser;
-pub use nom_parser::parse_ris as parse_ris_nom;
-pub use reference::Reference;
-pub use tag::Tag;
--- tests/appenzeller_herzog.rs
+++ tests/appenzeller_herzog.rs
@@ -1,14 +1,5 @@
-use ris::parse_ris_nom;
 use ris::RisParser;
 use std::fs;
-
-#[test]
-fn parse_nom() {
-    let file_path = "benches/files/Appenzeller-Herzog_2019.ris";
-    let contents = fs::read_to_string(file_path).unwrap();
-    let (_, output) = parse_ris_nom(&contents).unwrap();
-    assert_eq!(output.len(), 3453)
-}
 
 #[test]
 fn parse_handwritten() {
