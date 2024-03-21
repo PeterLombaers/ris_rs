11f223428fe99261b088db415c981990ec9e95c9
Jelle Teijema <j.j.teijema@gmail.com>
2024-03-21 15:21:40 +0100

    Add a GitHub workflow

--- benches/python_bench.py
+++ benches/python_bench.py
@@ -16,21 +16,21 @@ def test_appenzeller_herzog_python(benchmark):
     benchmark(load_file_rispy, "benches/files/Appenzeller-Herzog_2019.ris")
 
 
-def test_kwok_python(benchmark):
-    benchmark(load_file_rispy, "benches/files/Kwok_2020.ris")
+def test_brouwer_python(benchmark):
+    benchmark(load_file_rispy, "benches/files/Brouwer_2019.ris")
 
 
 def test_appenzeller_herzog_rust(benchmark):
     benchmark(load_file_rust, "benches/files/Appenzeller-Herzog_2019.ris")
 
 
-def test_kwok_rust(benchmark):
-    benchmark(load_file_rust, "benches/files/Kwok_2020.ris")
+def test_brouwer_rust(benchmark):
+    benchmark(load_file_rust, "benches/files/Brouwer_2019.ris")
 
 
 # def test_appenzeller_herzog_100_000_python(benchmark):
 #     benchmark(load_file_rispy, "benches/files/AH_100_000.ris")
 
 
-def test_appenzeller_herzog_100_000_rust(benchmark):
-    benchmark(load_file_rust, "benches/files/AH_100_000.ris")
+# def test_appenzeller_herzog_100_000_rust(benchmark):
+#     benchmark(load_file_rust, "benches/files/AH_100_000.ris")
--- tests/appenzeller_herzog.rs
+++ tests/appenzeller_herzog.rs
@@ -1,11 +1,21 @@
 use ris::RisParser;
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::fs;
 
 #[test]
 fn parse_handwritten() {
-    let file_path = "benches/files/Appenzeller-Herzog_2019.ris";
-    let contents = fs::read(file_path).unwrap();
+    let csv_file_path = "benches/files/Appenzeller-Herzog_2019.csv";
+    let ris_file_path = "benches/files/Appenzeller-Herzog_2019.ris";
+
+    let file = File::open(csv_file_path).unwrap();
+    let reader = io::BufReader::new(file);
+
+    let num_lines = reader.lines().count() - 1;
+
+    let contents = fs::read(ris_file_path).unwrap();
     let parser = RisParser::default();
     let output = parser.parse(&contents).unwrap();
-    assert_eq!(output.len(), 3453);
+
+    assert_eq!(output.len(), num_lines);
 }
